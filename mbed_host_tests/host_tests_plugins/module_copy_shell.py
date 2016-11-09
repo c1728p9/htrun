539a707d2742bdc17c79b3df704dbec93b764e3c
Russ Butler <russ.butler@arm.com>
2016-11-08 18:10:35 -0600

    Check for assert.txt and fail.txt

--- mbed_host_tests/host_tests_plugins/module_copy_shell.py
+++ mbed_host_tests/host_tests_plugins/module_copy_shell.py
@@ -63,52 +63,79 @@ class HostTestPluginCopyMethod_Shell(HostTestPluginBase):
         pooling_timeout = kwargs.get('pooling_timeout', 60)
 
         result = False
-        complete = False
-        if self.check_parameters(capability, *args, **kwargs):
-            if kwargs['image_path'] and kwargs['destination_disk']:
-                image_path = os.path.normpath(kwargs['image_path'])
-                destination_disk = os.path.normpath(kwargs['destination_disk'])
-                # Wait for mount point to be ready
-                # if mount point changed according to target_id use new mount point
-                # available in result (_, destination_disk) of check_mount_point_ready
-                mount_res, destination_disk = self.check_mount_point_ready(destination_disk, target_id=target_id, timeout=pooling_timeout)  # Blocking
-                # Prepare correct command line parameter values
-                image_base_name = basename(image_path)
-                destination_path = join(destination_disk, image_base_name)
-                if capability == 'shell':
-                    if os.name == 'nt': capability = 'copy'
-                    elif os.name == 'posix': capability = 'cp'
-                if capability == 'cp' or capability == 'copy' or capability == 'copy':
-                    copy_method = capability
-                    cmd = [copy_method, image_path, destination_path]
-                    if os.name == 'posix':
-                        result = self.run_command(cmd, shell=False)
-                        result = self.run_command(["sync"])
-                    else:
-                        result = self.run_command(cmd)
+        if not self.check_parameters(capability, *args, **kwargs):
+            return False
 
-                if result:
-                    checked_path = destination_path
-                    dismounted = False
-                    # Wait for ~30 seconds
-                    for i in range(3000):
-                        if os.path.exists(checked_path):
-                            sleep(0.01)
-                        else:
-                            dismounted = True
-                            break
-                    
-                    
-                    if not dismounted:
-                        self.print_plugin_error("Error: Disk failed to dismount")
-                    else:
-                        self.print_plugin_error("Disk dismounted, waiting for remount...")
-                        # Wait for mount point to be ready
-                        # if mount point changed according to target_id use new mount point
-                        # available in result (_, destination_disk) of check_mount_point_ready
-                        mount_res, destination_disk = self.check_mount_point_ready(destination_disk, target_id=target_id, timeout=pooling_timeout)  # Blocking
-                        complete = mount_res
-        return result and complete
+        image_path = os.path.normpath(kwargs['image_path'])
+        destination_disk = os.path.normpath(kwargs['destination_disk'])
+        # Wait for mount point to be ready
+        # if mount point changed according to target_id use new mount point
+        # available in result (_, destination_disk) of check_mount_point_ready
+        mount_res, destination_disk = self.check_mount_point_ready(destination_disk, target_id=target_id, timeout=pooling_timeout)  # Blocking
+        # Prepare correct command line parameter values
+        image_base_name = basename(image_path)
+        destination_path = join(destination_disk, image_base_name)
+        if capability == 'shell':
+            if os.name == 'nt': capability = 'copy'
+            elif os.name == 'posix': capability = 'cp'
+        if capability == 'cp' or capability == 'copy' or capability == 'copy':
+            copy_method = capability
+            cmd = [copy_method, image_path, destination_path]
+            if os.name == 'posix':
+                result = self.run_command(cmd, shell=False)
+                result = self.run_command(["sync"])
+            else:
+                result = self.run_command(cmd)
+        if not result:
+            return False
+
+        if not os.path.exists(destination_path):
+            self.print_plugin_info("Remount missed")
+
+        # Wait for dismount
+        result = self._wait_for_dismount(destination_path, 30)
+        if not result:
+            self.print_plugin_error("Error: Disk failed to dismount")
+            return False
+
+        # Wait for mount point to be ready
+        # if mount point changed according to target_id use new mount point
+        # available in result (_, destination_disk) of check_mount_point_ready
+        mount_res, destination_disk = self.check_mount_point_ready(destination_disk, target_id=target_id, timeout=pooling_timeout)  # Blocking
+        if not mount_res:
+            self.print_plugin_error("Error: Disk failed to remount")
+            return False
+
+        # Check for FAIL.TXT
+        fail_path = join(destination_disk, "FAIL.TXT")
+        if os.path.exists(fail_path):
+            with open(fail_path, "rb") as file_handle:
+                fail_data = file_handle.read()
+            self.print_plugin_error("Error flashing device: %s" % (fail_data,))
+
+        # Check for ASSERT.TXT and if it exists remove it
+        assert_path = join(destination_disk, "ASSERT.TXT")
+        if os.path.exists(assert_path):
+            with open(assert_path, "rb") as file_handle:
+                assert_data = file_handle.read()
+            self.print_plugin_error("Error: Assert on device: %s" % (assert_data,))
+            # Remove the assert - this will cause a remount
+            os.remove(assert_path)
+            self._wait_for_dismount(destination_disk, 30)
+            mount_res, destination_disk = self.check_mount_point_ready(destination_disk, target_id=target_id, timeout=pooling_timeout)
+            if not mount_res:
+                self.print_plugin_error("Error: Disk failed to remount after clearing assert")
+                return False
+
+        return True
+
+    def _wait_for_dismount(self, file_or_drive, timeout):
+        while os.path.exists(file_or_drive):
+            sleep(0.01)
+            timeout -= 0.01
+            if timeout < 0:
+                return False
+        return True
 
 
 def load_plugin():
